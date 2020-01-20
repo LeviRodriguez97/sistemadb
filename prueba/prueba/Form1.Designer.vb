2d8e2a39dbf8907a8fdcc416853d28fba5b82d06
LeviRodriguez97 <rodriguezlevi06@gmail.com>
2020-01-20 11:18:08 -0600

    segundo commit

--- prueba/prueba/Form1.Designer.vb
+++ prueba/prueba/Form1.Designer.vb
@@ -24,31 +24,40 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(71, 36)
+        Me.Label1.Location = New System.Drawing.Point(327, 29)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.Size = New System.Drawing.Size(80, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "HOLA"
+        Me.Label1.Text = "HOLA MUNDO"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(51, 88)
+        Me.Button1.Location = New System.Drawing.Point(325, 82)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(78, 43)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(325, 169)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(82, 20)
+        Me.TextBox1.TabIndex = 2
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
@@ -60,4 +69,5 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
 End Class
